6961d0820c33f4188465372dca60059f09c58ed0
Mikhail Emelianov <cheburusshka0303@gmail.com>
2023-11-02 01:16:23 +0500

    @Override added

--- src/main/java/ru/emelianov/oop/PersonWithCar.java
+++ src/main/java/ru/emelianov/oop/PersonWithCar.java
@@ -2,10 +2,13 @@ package ru.emelianov.oop;
 
 public class PersonWithCar implements Person {
 
+    @Override
     public Position getPosition() { return null; }
 
+    @Override
     public void walk(Position destination) {}
 
+    @Override
     public void useCar(Position destination) {
         Transport car = new Car();
         car.move(destination);
